0a9100f1ae3952c1795419db319916d2c4d7394f
Ayush Sharma <130376610+ayushxsharma0@users.noreply.github.com>
2023-10-20 19:23:23 +0530

    Time: 24 ms (22.49%) | Memory: 19.7 MB (51.20%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        // three conditions need to be satisfied for a valid sudoku
+        unordered_map<char,int> mp1 ;
+        unordered_map<char,int> mp2 ;
+        
+        int i,j ;
+        //check if all rows and cols are valid 
+        for(i =0; i<9; i++){
+            for(j=0;j<9;j++){
+
+                // to check whole row
+                if(board[i][j] != '.'){
+                    mp1[board[i][j]] ++ ;
+                }
+                //to check whole col
+
+                if(board[j][i] != '.'){
+                    mp2[board[j][i]] ++ ;
+                }
+
+                if(mp1[board[i][j]] > 1){   //if same num is more than once in same row
+                    return false ;
+                }
+
+                if(mp2[board[j][i]] > 1){   //if same num is more than once in same col
+                    return false ;
+                }
+            }
+            mp1.clear() ;
+            mp2.clear() ;
+        }
+        mp1.clear() ;
+        // check each 3x3 smaller matrices
+        for(i = 0 ;i<9; i+=3){
+            for(j=0; j<9;j+=3){
+                for(int r =i ; r<i+3;r++){
+                    for(int c = j;c<j+3;c++){
+                        if(board[r][c] != '.'){
+                            mp1[board[r][c]] ++ ;
+                        }
+                        if(mp1[board[r][c]] > 1){
+                            return false ;
+                        }
+                    }
+                }
+                mp1.clear() ;
+            }
+        }
+
+        return true ;
+    }
+};
