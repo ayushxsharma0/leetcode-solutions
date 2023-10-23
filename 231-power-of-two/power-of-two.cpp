4c7a532ad26fbdd12c852c218145d515b6b915c5
Ayush Sharma <130376610+ayushxsharma0@users.noreply.github.com>
2023-10-23 19:02:46 +0530

    Time: 0 ms (100.00%) | Memory: 6.3 MB (48.27%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -1,11 +1,18 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        //using loops
-        if(n<=0) return 0 ;
-        while(n%2 ==0){
-            n/=2 ;
-        }
-        return n==1 ;
+        // //using loops
+        // if(n<=0) return 0 ;
+        // while(n%2 ==0){
+        //     n/=2 ;
+        // }
+        // return n==1 ;
+
+        //using bit manipulation
+        // for every power of 2 lets say n 
+        // n and n-1 == 0 
+        //this can be only observed by writing numbers in binary digits
+
+        return (n>0 && ((n & (n-1)) == 0)) ;
     }
 };
