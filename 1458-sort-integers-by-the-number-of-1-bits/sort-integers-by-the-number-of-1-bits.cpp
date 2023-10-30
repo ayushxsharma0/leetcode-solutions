7e2f7c100847192075c6c1fc694ecf90950695b1
Ayush Sharma <130376610+ayushxsharma0@users.noreply.github.com>
2023-10-30 09:59:05 +0530

    Time: 8 ms (53.22%) | Memory: 10.5 MB (66.29%) - LeetSync

--- 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.cpp
+++ 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
-    static int numbit(int a){
-        int count =0;
-        while(a){
-            count++ ;
-            a &= a-1 ;
-        }
-        return count ;
-    }
+    // brian kernighan algorithm to find number of 1 bits in a given number
+    // static int numbit(int a){
+    //     int count =0;
+    //     while(a){
+    //         count++ ;
+    //         a &= a-1 ;
+    //     }
+    //     return count ;
+    // }
 
     static bool comp(int x, int y){
-        int countbit = numbit(x) ;
-        int countbit2 = numbit(y) ;
+        int countbit = __builtin_popcount(x) ;  // built in function to count number of set bit(1)
+        int countbit2 = __builtin_popcount(y) ;
 
         if(countbit == countbit2){
             return y>x ;
@@ -19,6 +20,7 @@ public:
         return countbit2>countbit ;
     }    
     vector<int> sortByBits(vector<int>& arr) {
+        
         sort(arr.begin(),arr.end(),comp) ;
         return arr ;
     }
