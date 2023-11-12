39890cd25a79d8361e1899cce5b1b471d3f46415
Ayush Sharma <130376610+ayushxsharma0@users.noreply.github.com>
2023-11-12 11:29:12 +0530

    Time: 21 ms (67.08%) | Memory: 25.9 MB (38.60%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
-    void reverse(int l, int h, vector<int>&arr){
-        while(h>l){
-            swap(arr[l++],arr[h--]) ;
-        }
-    }
     void rotate(vector<int>& nums, int k) {
-        int n = nums.size() ;
-        k=k% n ;
-        reverse(0,n-1,nums) ;
-        reverse(0,k-1,nums) ;
-        reverse(k,n-1,nums) ;
-    } 
+        //create a new array and put totated array in it
+        vector<int> v(nums.size(),0) ;
+        for(int i =0;i<nums.size();i++){
+            v[(i+k)%nums.size()] = nums[i] ;
+        }
+        nums = v ;
+    }
+
+    // // reverse the array in particular order
+    // reverse(0,n-1,arr) ;
+    // reverse(0,k-1,arr) ;
+    // reverse(k,n-1,arr) ;
+    // where reverse is the function to reverse the array
 };
