7f16e32276a48f77969f3d083fc8958cef952416
Ayush Sharma <130376610+ayushxsharma0@users.noreply.github.com>
2023-11-19 11:38:34 +0530

    Time: 153 ms (79.76%) | Memory: 83.1 MB (68.40%) - LeetSync

---
+++ 2016-reduction-operations-to-make-the-array-elements-equal/reduction-operations-to-make-the-array-elements-equal.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int reductionOperations(vector<int>& nums) {
+        int ans = 0 ;
+        int count = 0 ;
+        sort(nums.begin(), nums.end()) ;
+        int curr = nums[0];
+        for(int i =0; i<nums.size();i++){
+            if(curr != nums[i]) count +=1 ; curr = nums[i] ;
+            ans += count ;
+        }
+        return ans ;
+    }
+};
