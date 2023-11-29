7e01f53f938b7dcf20a0d91f5d8bf7b8972a030c
Ayush Sharma <130376610+ayushxsharma0@users.noreply.github.com>
2023-11-29 19:51:21 +0530

    Time: 4 ms (30.61%) | Memory: 6.6 MB (9.00%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.cpp
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -3,6 +3,7 @@ public:
     int hammingWeight(uint32_t n) {
         int count =0 ;
         //brian kermingham algorithm
+        //when we substract 1 from binary reresent of a number it reverses all vits after rightmost set bit
         while(n){
             n= n&(n-1) ;
             count++ ;
