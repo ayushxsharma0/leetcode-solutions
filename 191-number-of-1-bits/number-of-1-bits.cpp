d69e60cca075d880ad101ca057b46d1ffc49fa98
Ayush Sharma <130376610+ayushxsharma0@users.noreply.github.com>
2023-11-29 19:50:33 +0530

    Time: 4 ms (30.61%) | Memory: 6.2 MB (94.23%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.cpp
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int hammingWeight(uint32_t n) {
         int count =0 ;
+        //brian kermingham algorithm
         while(n){
             n= n&(n-1) ;
             count++ ;
