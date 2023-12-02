ad1585770ae20deec37bb75e03cc605a9a588697
Ayush Sharma <130376610+ayushxsharma0@users.noreply.github.com>
2023-12-02 19:33:25 +0530

    Time: 188 ms (11.95%) | Memory: 37.3 MB (41.66%) - LeetSync

---
+++ 1112-find-words-that-can-be-formed-by-characters/find-words-that-can-be-formed-by-characters.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int countCharacters(vector<string>& words, string chars) {
+        //map to store frequency of character in chars
+        unordered_map<char,int> mp ;
+        for(auto i : chars){
+            mp[i] ++ ;
+        }
+        string s ;
+        int ans =0 ;
+        bool b=true ;
+        unordered_map<char,int> mapp ;
+        for(int i =0; i<words.size();i++){
+            s = words[i] ;
+            for(auto i : s){
+                mapp[i] ++ ;
+            }
+            for(auto i : s){
+                if(mapp[i] > mp[i]){
+                    b = false ;
+                }
+            }
+            if(b) ans+= s.size() ;
+            b= true ;
+            mapp.clear() ;
+        }
+        return ans ;
+    }
+};
