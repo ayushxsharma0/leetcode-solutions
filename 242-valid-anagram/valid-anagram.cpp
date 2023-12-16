57afdc8930dd1bddf769f55de32b7b81738e882c
Ayush Sharma <130376610+ayushxsharma0@users.noreply.github.com>
2023-12-16 11:25:17 +0530

    Time: 6 ms (80.64%) | Memory: 7.8 MB (45.96%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -2,11 +2,26 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         if(s.size() != t.size()) return false ;
-        sort(s.begin(), s.end()) ;
-        sort(t.begin(),t.end()) ;
+        
+        // nlogn time complexity solution
+        // sort(s.begin(), s.end()) ;
+        // sort(t.begin(),t.end()) ;
 
-        for(int i =0; i<s.size();i++){
-            if(s[i] != t[i]) return false ;
+        // for(int i =0; i<s.size();i++){
+        //     if(s[i] != t[i]) return false ;
+        // }
+        // return true ;
+
+        // n time complexity
+        unordered_map<char,int> mp ;
+        for(auto i : s){
+            mp[i] ++ ;
+        }
+        for(auto i : t){
+            mp[i] -- ;
+        }
+        for(auto i : mp){
+            if(i.second != 0) return false ;
         }
         return true ;
     }
