35ea36396a64eee71986954b5fab27c552cd7521
Ayush Sharma <130376610+ayushxsharma0@users.noreply.github.com>
2023-12-26 08:32:40 +0530

    Time: 1828 ms (5.01%) | Memory: 303.6 MB (5.00%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -9,13 +9,12 @@ public:
         //fix one number
         for(int i =0;i<n;i++){
             int j = i+1 ; int k = n-1 ;
-            int x = nums[i] ;
             while(j<k){
-                
-                    if((x+nums[j]+nums[k]) == 0){
-                        s.insert({x,nums[j],nums[k]}) ;
+                int sum = nums[i] + nums[j]+nums[k] ;
+                    if(sum == 0){
+                        s.insert({nums[i],nums[j],nums[k]}) ;
                     }
-                    if((x+nums[j]+nums[k]) > 0){
+                    if(sum > 0){
                         k-- ;
                     }
                     else{
