c158d82d3729e96cc9ebdd166ec2257d1d5b6554
Ayush Sharma <130376610+ayushxsharma0@users.noreply.github.com>
2024-01-28 11:15:30 +0530

    Time: 79 ms (49.98%) | Memory: 50.6 MB (5.76%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -19,20 +19,38 @@ public:
         // return count ;
 
         //using prefix sum array
-        int n = nums.size() ;
-        int count = 0; 
-        vector<int> arr(n+1) ;
-        arr[0] = 0 ;
-        for(int i = 1 ;i<=n;i++){
-            arr[i] = arr[i-1] + nums[i -1] ;
-        }
-        //now calculate all subarrays
+        // int n = nums.size() ;
+        // int count = 0; 
+        // vector<int> arr(n+1) ;
+        // arr[0] = 0 ;
+        // for(int i = 1 ;i<=n;i++){
+        //     arr[i] = arr[i-1] + nums[i -1] ;
+        // }
+        // //now calculate all subarrays
 
-        for(int i =0;i<n;i++){
-            for(int j=i+1;j<=n;j++){
-                int sum = arr[j] - arr[i] ;
-                if(sum == k) count++ ;
-            }
+        // for(int i =0;i<n;i++){
+        //     for(int j=i+1;j<=n;j++){
+        //         int sum = arr[j] - arr[i] ;
+        //         if(sum == k) count++ ;
+        //     }
+        // }
+        // return count ;
+
+        //most optimised approach
+        //use a hashmap to store sum and number of times it appears
+
+        int n = nums.size() ;
+        unordered_map<int,int> mp ;
+        mp[0] = 1 ;
+        int count = 0;
+        int sum = 0 ;
+        for(int i=0 ;i< n;i++){
+            sum += nums[i] ;
+            //check if map contains sum -k
+            if(mp[sum - k] != 0){
+                count += mp[sum - k] ;
+            } 
+            mp[sum] ++ ;
         }
         return count ;
     }
