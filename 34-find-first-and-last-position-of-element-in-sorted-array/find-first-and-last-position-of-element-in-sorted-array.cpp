1ae59e3150d157a1fffb170cb6163fc9b5c33fc8
Ayush Sharma <130376610+ayushxsharma0@users.noreply.github.com>
2024-01-29 12:21:45 +0530

    Time: 3 ms (92.65%) | Memory: 16 MB (14.79%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,22 +1,65 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int> ans = {-1,-1} ;
+        // vector<int> ans = {-1,-1} ;
 
-        int i =0; int j =nums.size()-1 ;
-        for(;j>=0;j--){
-            if(nums[j] == target){
-                ans[1] = j ;
-                break ;
+        // int i =0; int j =nums.size()-1 ;
+        // for(;j>=0;j--){
+        //     if(nums[j] == target){
+        //         ans[1] = j ;
+        //         break ;
+        //     }
+        // }
+        // for(;i<nums.size();i++){
+        //     if(nums[i] == target){
+        //         ans[0] = i ;
+        //         break ;
+        //     }
+
+        // }
+        // return ans ;
+        //this works though it is O(n) approach
+
+        //using binary search to get in O(logn) time
+        int firstidx = -1 ;
+        int lastidx = -1 ;
+        int s =0 ;int e = nums.size() -1 ;
+
+        //to find start index
+        while(e>=s){
+            int mid = (e+s)/2 ;
+
+            if(nums[mid] == target){
+                firstidx = mid ;
+                e = mid-1 ;
+            }
+            else if(nums[mid] > target){
+                e = mid-1 ;
+            }
+            else{
+                s = mid+1 ;
             }
         }
-        for(;i<nums.size();i++){
-            if(nums[i] == target){
-                ans[0] = i ;
-                break ;
-            }
 
+        if(firstidx == -1) return {-1,-1} ;
+        //to find last index
+        s = 0;  e = nums.size() -1 ;
+    
+        while(e>=s){
+            int mid = (e+s)/2 ;
+
+            if(nums[mid] == target){
+                lastidx = mid ;
+                s = mid+ 1 ;
+            }
+            else if(nums[mid] > target){
+                e = mid -1 ;
+            }
+            else{
+                s = mid+1 ;
+            }
         }
-        return ans ;
+        return {firstidx,lastidx} ;
+
     }
 };
