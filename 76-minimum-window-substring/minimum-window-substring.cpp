1a35abf8d4aeab8ad1551f42ef1fc542b1cd25c8
Ayush Sharma <130376610+ayushxsharma0@users.noreply.github.com>
2024-02-04 11:42:59 +0530

    Time: 16 ms (50.95%) | Memory: 10.3 MB (15.00%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,42 +1,37 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        int m = s.size() ;
-        int counter = t.size() ;
-        int i=0; int j=0 ;
-        unordered_map<char,int> map ;
+        int n = s.size() ;
+        int count = t.size() ;
 
+        unordered_map<char,int> mp ;
         for(auto i : t){
-            map[i] ++ ;
+            mp[i]++ ;
         }
-        int length = INT_MAX ;
-        int start =0 ;
-        while(j<m){
-            //if char of t is found in s
-            if(map[s[j]]>0){
-                counter-- ;
-            }
-            map[s[j]] -- ;
-            j++ ;
-            //untill window is found
+        int len = INT_MAX ;
+        int i =0; int j =0; int start ;
 
-            while(counter==0){
-                if(j-i  < length){
+        while(j<n){
+            //get the required window
+            if(mp[s[j]] >0){
+                count-- ;
+            }
+            mp[s[j]]-- ;
+            j++ ;
+            //when we get the required window
+
+            while(count == 0){
+                if(j-i < len){
                     start = i ;
-                    length = j-i ;
+                    len = j-i ;
                 }
-                    map[s[i]]++ ;
-                    if(map[s[i]] >0){
-                    counter++ ;
+                mp[s[i]]++ ;
+                if(mp[s[i]] > 0){
+                    count++ ;
                 }
                 i++ ;
             }
         }
-        
-
-        if(length != INT_MAX){
-            return s.substr(start,length) ;
-        }
-        return "" ;
+        return len!= INT_MAX ? s.substr(start,len) : "" ;
     }
 };
