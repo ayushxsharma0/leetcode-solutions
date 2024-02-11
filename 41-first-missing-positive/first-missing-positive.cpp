88de92125ade75d8c757d5b7936f52f8afc64f76
Ayush Sharma <130376610+ayushxsharma0@users.noreply.github.com>
2024-02-11 10:05:26 +0530

    Time: 31 ms (98.87%) | Memory: 43.4 MB (43.13%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -2,22 +2,52 @@ class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
         //use a set to store positive numbers in ascending order
-        set<int> s ;
+        // set<int> s ;
 
-        for(auto i : nums){
-            if(i>0){
-                s.insert(i) ;
+        // for(auto i : nums){
+        //     if(i>0){
+        //         s.insert(i) ;
+        //     }
+        // }
+        // int x = 1 ;
+        // for(auto i : s){
+        //     if(i == x){
+        //         x++ ;
+        //     }
+        //     else{
+        //         break ;
+        //     }
+        // }
+        // return x ;
+
+        // the above approach has O(n) T.C and O(n) S.C.
+
+        //O(n) and O(1) solution !!
+
+        int n = nums.size() ;
+        for(int i = 0;i<n;i++){
+            if(nums[i] <=0 || nums[i] > n){
+                nums[i] = n+1 ;
             }
         }
-        int x = 1 ;
-        for(auto i : s){
-            if(i == x){
-                x++ ;
-            }
-            else{
-                break ;
+        //now mark the array as needed
+
+        for(int i =0;i <n;i++){
+            int num = abs(nums[i]) ;
+            if(num >n) continue ;
+
+            num-- ;
+            if(nums[num] > 0){
+                nums[num] *= -1 ;
             }
         }
-        return x ;
+
+        //check the first num which is non negative
+        for(int i =0; i<n ;i++){
+            if(nums[i] >= 0){
+                return i +1 ; //i.e. we didnt find this number in our previous loop
+            }
+        }
+        return n+1 ;
     }
 };
