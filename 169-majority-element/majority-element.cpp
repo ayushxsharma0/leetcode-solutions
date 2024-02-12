403db5813fc27da9521d98cf2cb07c1132f04d08
Ayush Sharma <130376610+ayushxsharma0@users.noreply.github.com>
2024-02-12 08:33:37 +0530

    Time: 15 ms (53.21%) | Memory: 22 MB (24.81%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -2,16 +2,34 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         //using extra space
-        int n = nums.size() ; 
-        unordered_map<int,int> mp ;
-        for(auto i : nums){
-            mp[i]++ ;
-        }
-        for(auto it : mp){
-            if(it.second > floor(n/2)){
-                return it.first ;
+        // int n = nums.size() ; 
+        // unordered_map<int,int> mp ;
+        // for(auto i : nums){
+        //     mp[i]++ ;
+        // }
+        // for(auto it : mp){
+        //     if(it.second > floor(n/2)){
+        //         return it.first ;
+        //     }
+        // }
+        // return 0 ;
+
+        //boyer moore algorithm
+        int votes = 0 ;
+        int curr =nums[0] ;
+
+        for(int i =0;i<nums.size() ;i++){
+            if(curr == nums[i]){
+                votes ++ ;
+            }
+            else{
+                votes-- ;
+            }
+            if(votes == 0){
+                curr = nums[i] ;
+                votes = 1 ;
             }
         }
-        return 0 ;
+        return curr ;
     }
 };
