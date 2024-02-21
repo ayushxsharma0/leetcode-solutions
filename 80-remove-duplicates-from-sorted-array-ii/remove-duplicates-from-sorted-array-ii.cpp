159e18da7a84b960870f647f204b965e7f693f86
Ayush Sharma <130376610+ayushxsharma0@users.noreply.github.com>
2024-02-21 19:21:59 +0530

    Time: 8 ms (25.52%) | Memory: 13.5 MB (7.37%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
@@ -1,22 +1,48 @@
+// class Solution {
+// public:
+//     int removeDuplicates(vector<int>& nums) {
+//         //using extra space 
+        
+//         // int n = nums.size() ;
+//         // map<int,int> mp ;
+
+//         // for(auto i : nums){
+//         //     mp[i]++ ;
+//         // }
+//         // int idx = 0 ;
+//         // for(auto it : mp){
+//         //     if(it.second >= 2){
+//         //         nums[idx++] = it.first ;
+//         //         nums[idx++] = it.first ;
+//         //     }
+//         //     else{
+//         //         nums[idx++] = it.first ;
+//         //     }
+//         // }
+//         // return idx ;
+//     }
+// };
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int n = nums.size() ;
-        map<int,int> mp ;
+        int k = 2; // define at most k times of duplicate numbers
+        
+        int l = 1, count = 1;
 
-        for(auto i : nums){
-            mp[i]++ ;
-        }
-        int idx = 0 ;
-        for(auto it : mp){
-            if(it.second >= 2){
-                nums[idx++] = it.first ;
-                nums[idx++] = it.first ;
+        for(int r = 1; r < nums.size(); r++){
+            if(nums[r] == nums[r-1]){
+                if(count < k)
+                    nums[l++] = nums[r];
+                count++;
             }
-            else{
-                nums[idx++] = it.first ;
+            else {
+                count = 1;
+                nums[l++] = nums[r];
             }
         }
-        return idx ;
+
+        return l;
     }
 };
+
+
