426dc947c2b27ff8a5d20fceb9c86f3dd7e18427
Ayush Sharma <130376610+ayushxsharma0@users.noreply.github.com>
2024-03-12 20:07:48 +0530

    Time: 12 ms (24.37%) | Memory: 15 MB (16.76%) - LeetSync

--- 1267-remove-zero-sum-consecutive-nodes-from-linked-list/remove-zero-sum-consecutive-nodes-from-linked-list.cpp
+++ 1267-remove-zero-sum-consecutive-nodes-from-linked-list/remove-zero-sum-consecutive-nodes-from-linked-list.cpp
@@ -11,25 +11,53 @@
 class Solution {
 public:
     ListNode* removeZeroSumSublists(ListNode* head) {
-        //O(n2) approach
-        ListNode* first = new ListNode(0,head) ;
-        ListNode* start = first ;   
+        // O(n2) approach
+        //  ListNode* first = new ListNode(0,head) ;
+        //  ListNode* start = first ;
 
-        while(start){
-            //get prefix sum and iterate for each node
-            int prefixsum = 0 ;
-            ListNode* end = start->next ;
+        // while(start){
+        //     //get prefix sum and iterate for each node
+        //     int prefixsum = 0 ;
+        //     ListNode* end = start->next ;
 
-            while(end){
-                prefixsum += end->val ;
+        //     while(end){
+        //         prefixsum += end->val ;
 
-                if(prefixsum == 0){
-                    start->next = end->next ;
-                }
-                end = end->next ;
-            }
-            start = start->next ;
+        //         if(prefixsum == 0){
+        //             start->next = end->next ;
+        //         }
+        //         end = end->next ;
+        //     }
+        //     start = start->next ;
+        // }
+        // return first->next ;
+
+        // O(n) approach
+
+        ListNode* first = new ListNode(0, head);
+        ListNode* dummy = first;
+
+        unordered_map<int, ListNode*> mp;
+        mp[0] = first;
+        int psum = 0;
+        while (dummy) {
+            psum += dummy->val;
+            mp[psum] = dummy;
+            dummy = dummy->next;
         }
+
+        // now iterate and delete
+        psum = 0;
+        dummy = first;
+
+        while (dummy) {
+            psum += dummy->val;
+            dummy->next =
+                mp[psum]->next; // if there is a node which has psum eqaul to current
+                          // psum then there difference will be zero
+            dummy = dummy->next;
+        }
+
         return first->next ;
     }
 };
