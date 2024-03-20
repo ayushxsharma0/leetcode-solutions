bfcc88a6ce5ade9336d9caf576eb68fe03f82206
Ayush Sharma <130376610+ayushxsharma0@users.noreply.github.com>
2024-03-20 12:22:01 +0530

    Time: 125 ms (42.11%) | Memory: 82 MB (63.84%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,35 +1,35 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        //we take a monotonic stack with elements in non decreasing order
+        stack<pair<int,int>> stk ;  //[index,height]
 
-        // [index,height]
-        stack<pair<int,int>> stk ;
         int res = 0 ;
 
-        for(int i =0;i<heights.size();i++){
+        for(int i = 0;i<heights.size(); i++){
             int start = i ;
 
+            //if at any index we get height less than top of stack then we can't continue the prev height
             while(!stk.empty() && stk.top().second > heights[i]){
                 int index = stk.top().first ;
-                int width = i-index ;
                 int height = stk.top().second ;
+                int width = i- index ;
                 stk.pop() ;
 
                 res = max(res, height*width) ;
                 start = index ;
             }
-
             stk.push({start,heights[i]}) ;
         }
-        
-        while(!stk.empty()){
-            int width = heights.size() - stk.top().first ;
-            int height = stk.top().second ;
-            stk.pop() ;
-            res = max(res,height*width) ;
-        }
 
+        //if the result is present in the stack
+        while(!stk.empty()){
+            int h = stk.top().second ;
+            int w = heights.size() - stk.top().first ;
+
+            stk.pop() ;
+
+            res = max(res,h*w) ;
+        }
         return res ;
     }
 };
