be44cc7519b3f3dea9ac0c02baffebe66d3c6a09
Ayush Sharma <130376610+ayushxsharma0@users.noreply.github.com>
2024-03-20 12:08:43 +0530

    Time: 3 ms (41.01%) | Memory: 7.3 MB (35.73%) - LeetSync

--- 365-water-and-jug-problem/water-and-jug-problem.cpp
+++ 365-water-and-jug-problem/water-and-jug-problem.cpp
@@ -1,21 +1,34 @@
-class Solution {
+class Solution
+{
 public:
-    bool dfs(int total, int jug1, int jug2, int jug, int target, vector<int>&vis)
+    bool canMeasureWater(int x, int y, int z)
     {
-        if(jug>total || jug<0 || vis[jug]==1)
-            return false;
-        vis[jug]=1;
-        if(jug == target)
+        if (x + y == z || x == z || y == z)
             return true;
-        bool a = dfs(total, jug1, jug2, jug+jug1, target, vis);
-        bool b = dfs(total, jug1, jug2, jug-jug1, target, vis);
-        bool c = dfs(total, jug1, jug2, jug+jug2, target, vis);
-        bool d = dfs(total, jug1, jug2, jug-jug2, target, vis);
-        return a||b||c||d;
-    }
-    bool canMeasureWater(int jug1Capacity, int jug2Capacity, int targetCapacity) {
-        int total = jug1Capacity + jug2Capacity;
-        vector<int>vis(total+1, 0);
-        return dfs(total, jug1Capacity, jug2Capacity, 0, targetCapacity, vis);
+        if (x + y < z)
+            return false;
+        int m = x + y; // max capacity
+        vector<int> v = {x, -x, y, -y};
+        queue<int> q;
+        q.push(0);
+        vector<int> vis(m + 1, 0);
+        vis[0] = 1;
+        while (!q.empty())
+        {
+            int curr = q.front();
+            q.pop();
+            if (curr == z)
+                return true;
+            for (int i = 0; i < 4; i++)
+            {
+                int k = curr + v[i];
+                if (k >= 0 && k <= m && vis[k] == 0)
+                {
+                    q.push(k);
+                    vis[k] = 1;
+                }
+            }
+        }
+        return false;
     }
 };
