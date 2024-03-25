e240feff88c19335f81d9827fcfa572672711e24
Ayush Sharma <130376610+ayushxsharma0@users.noreply.github.com>
2024-03-25 10:02:12 +0530

    Time: 31 ms (94.91%) | Memory: 35.9 MB (77.98%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
@@ -3,15 +3,28 @@ public:
     vector<int> findDuplicates(vector<int>& nums) {
         //O(n) space and time using hashmap
 
-        unordered_map<int,int> mp ;
-        for(auto i : nums){
-            mp[i]++ ;
-        }
+        // unordered_map<int,int> mp ;
+        // for(auto i : nums){
+        //     mp[i]++ ;
+        // }
+        // vector<int> ans ;
+        // for(auto i : mp){
+        //     if(i.second == 2){
+        //         ans.push_back(i.first) ;
+        //     }
+        // }
+        // return ans ;
+
+        //O(1) approach -> basically marking index which is already seen
+        int n = nums.size() ;
         vector<int> ans ;
-        for(auto i : mp){
-            if(i.second == 2){
-                ans.push_back(i.first) ;
+
+        for(int i = 0 ;i<n;i++){
+            int idx = abs(nums[i]) - 1 ;
+            if(nums[idx] < 0){
+                ans.push_back(idx+1) ;
             }
+            nums[idx] *= -1 ;
         }
         return ans ;
     }
